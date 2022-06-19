23c58cd3e25c378e68caf4ccc1d3444d44318cbf
unknown <vivienduvernois@gmail.com>
2022-06-19 19:43:13 +0200

    ajout GCP dans pom.xml

--- src/main/java/com/petitions/restapi/PetitionsApplication.java
+++ src/main/java/com/petitions/restapi/PetitionsApplication.java
@@ -1,5 +1,12 @@
 package com.petitions.restapi;
 
+import org.springframework.boot.SpringApplication;
+
 public class PetitionsApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetitionsApplication.class, args);
+	}
 
 }
