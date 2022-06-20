064a916ad643911274f44de6df801a974bc3c205
unknown <vivienduvernois@gmail.com>
2022-06-20 18:17:19 +0200

    correction des références circulaires

--- src/main/java/com/petitions/restapi/controller/UserController.java
+++ src/main/java/com/petitions/restapi/controller/UserController.java
@@ -14,8 +14,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.petitions.restapi.model.User;
 import com.petitions.restapi.service.UserService;
 
@@ -33,6 +31,71 @@ public class UserController {
 	@PostMapping("/user")
     public User saveUser(@RequestBody User user) {
         return userService.saveUser(user);
+    }
+	
+	/**
+	 * Read - Get all users
+	 * @return An Iterable object of User full filled
+	 */
+	@GetMapping("/users")
+	public Iterable<User> getUsers(){
+		return userService.getUsers();
+	}
+	
+	/**
+	 * Read - Get one user 
+	 * @param id The id of the user
+	 * @return A User object full filled
+	 */
+	@GetMapping("/user/{id}")
+	public User getUser(@PathVariable("id") final Long id) {
+		Optional<User> user = userService.getUser(id);
+		if(user.isPresent()) {
+			return user.get();
+		} else {
+			return null;
+		}
+    }
+    
+	/**
+	 * Read - Get all users with this first name and this last name
+	 * @param firstName The first name of the user
+	 * @param lastName The last name of the user
+	 * @return An Employee object full filled
+	 */
+	@GetMapping("/users/{firstName}/{lastName}")
+	List<User> getUsersByLastNameAndFirstName(@PathVariable("firstName") String firstName, @PathVariable("lastName") String lastName){
+		return userService.getUsersByLastNameAndFirstName(firstName, lastName);
+	};
+	
+	/**
+	 * Read - Get one employee
+	 * @param id The id of the employee
+	 * @return An Employee object full filled
+	 */
+	@GetMapping("/user/{mail}")
+	List<User> getUsersByMail(@PathVariable("mail") String mail){
+		return userService.getUsersByMail(mail);
+	};
+	
+	/**
+	 * Delete - Delete one employee 
+	 * @param id The id of the employee
+	 */	
+	@DeleteMapping("/user/{id}")
+    public void deleteUser(@PathVariable("id") final Long id) {
+    	userService.deleteUser(id);
+    }
+	
+	//SHELL METHODS
+	
+	/**
+	 * Delete - Delete one employee 
+	 * @param id The id of the employee
+	 */	
+	@ShellMethod("Removes all users")
+    public void deleteAllUsers() {
+    	userService.deleteAllUsers();
     }
 	
 	/**
@@ -57,72 +120,4 @@ public class UserController {
 		//visual confirmation
 		return listUsers.toString();
 	}
-	
-	/**
-	 * Read - Get all users
-	 * @return An Iterable object of User full filled
-	 */
-	@ShellMethod
-	@GetMapping("/users")
-	public Iterable<User> getUsers(){
-		return userService.getUsers();
-	}
-	
-	/**
-	 * Read - Get one user 
-	 * @param id The id of the user
-	 * @return A User object full filled
-	 */
-	@ShellMethod
-	@GetMapping("/user/{id}")
-	public User getUser(@PathVariable("id") final Long id) {
-		Optional<User> user = userService.getUser(id);
-		if(user.isPresent()) {
-			return user.get();
-		} else {
-			return null;
-		}
-    }
-    
-	/**
-	 * Read - Get all users with this first name and this last name
-	 * @param firstName The first name of the user
-	 * @param lastName The last name of the user
-	 * @return An Employee object full filled
-	 */
-	@ShellMethod
-	@GetMapping("/users/{firstName}/{lastName}")
-	List<User> getUsersByLastNameAndFirstName(@PathVariable("firstName") String firstName, @PathVariable("lastName") String lastName){
-		return userService.getUsersByLastNameAndFirstName(firstName, lastName);
-	};
-	
-	/**
-	 * Read - Get one employee
-	 * @param id The id of the employee
-	 * @return An Employee object full filled
-	 */
-	@ShellMethod
-	@GetMapping("/user/{mail}")
-	List<User> getUsersByMail(@PathVariable("mail") String mail){
-		return userService.getUsersByMail(mail);
-	};
-	
-	/**
-	 * Delete - Delete one employee 
-	 * @param id The id of the employee
-	 */	
-	@ShellMethod
-	@DeleteMapping("/user/{id}")
-    public void deleteUser(@PathVariable("id") final Long id) {
-    	userService.deleteUser(id);
-    }
-	
-	/**
-	 * Delete - Delete one employee 
-	 * @param id The id of the employee
-	 */	
-	@ShellMethod("Removes all users")
-    public void deleteAllUsers() {
-    	userService.deleteAllUsers();
-    }
 }
