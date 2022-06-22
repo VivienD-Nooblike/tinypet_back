3c033f3041c3ab555ecf5f69070753d88237377d
unknown <vivienduvernois@gmail.com>
2022-06-22 18:02:21 +0200

    correction génération des pétitions v4

--- src/main/java/com/petitions/restapi/service/SignatureService.java
+++ src/main/java/com/petitions/restapi/service/SignatureService.java
@@ -3,10 +3,15 @@ package com.petitions.restapi.service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.petitions.restapi.model.Signature;
 import com.petitions.restapi.repository.SignatureRepository;
 
+import lombok.Data;
+
+@Data
+@Service
 public class SignatureService {
 	@Autowired
 	SignatureRepository signatureRepository;
